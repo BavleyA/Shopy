a332c8736b35b06fec071c47364c7f3ae001fdb4
BavleyA <paula3del@gmail.com>
2024-11-28 18:44:04 +0200

    add shared styles (themes)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:shop_app/shared/cubit/appCubit.dart';
 import 'package:shop_app/shared/cubit/appCubitStates.dart';
+import 'package:shop_app/shared/styles/themes.dart';
 
 import 'modules/on_boarding/on_boarding_screen.dart';
 
@@ -27,82 +28,8 @@ class MyApp extends StatelessWidget {
         builder: (context ,states) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primarySwatch: Colors.red,
-              scaffoldBackgroundColor: Colors.white,
-              appBarTheme: AppBarTheme(
-                titleSpacing: 20.0,
-                systemOverlayStyle: SystemUiOverlayStyle(
-                  statusBarColor: Colors.white,
-                  statusBarIconBrightness: Brightness.dark,
-                ),
-                backgroundColor: Colors.white,
-                elevation: 0.0,
-                titleTextStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                iconTheme: IconThemeData(
-                  color: Colors.black,
-                ),
-              ),
-              floatingActionButtonTheme: FloatingActionButtonThemeData(
-                backgroundColor: Colors.red,
-              ),
-              bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                selectedItemColor: Colors.red,
-                unselectedItemColor: Colors.grey,
-                elevation: 20.0,
-                type: BottomNavigationBarType.fixed,
-                backgroundColor: Colors.white,
-              ),
-              textTheme: TextTheme(
-                bodyLarge: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            darkTheme: ThemeData(
-              primarySwatch: Colors.red,
-              scaffoldBackgroundColor: HexColor('333739'),
-              appBarTheme: AppBarTheme(
-                titleSpacing: 20.0,
-                systemOverlayStyle: SystemUiOverlayStyle(
-                  statusBarColor: HexColor('333739'),
-                  statusBarIconBrightness: Brightness.light,
-                ),
-                backgroundColor: HexColor('333739'),
-                elevation: 0.0,
-                titleTextStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                iconTheme: IconThemeData(
-                  color: Colors.white,
-                ),
-              ),
-              floatingActionButtonTheme: FloatingActionButtonThemeData(
-                backgroundColor: Colors.red,
-              ),
-              bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                selectedItemColor: Colors.red,
-                unselectedItemColor: Colors.grey,
-                elevation: 20.0,
-                type: BottomNavigationBarType.fixed,
-                backgroundColor: HexColor('333739'),
-              ),
-              textTheme: TextTheme(
-                bodyLarge: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white
-                ),
-              ),
-            ),
+            theme: lightTheme,
+            darkTheme: darkTheme,
             themeMode: AppCubit.get(context).isDark ? ThemeMode.dark : ThemeMode.light,
             home: OnBoardingScreen(),
           );
---
+++ lib/shared/styles/themes.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+ThemeData darkTheme = ThemeData(
+  primarySwatch: Colors.red,
+  scaffoldBackgroundColor: HexColor('333739'),
+  appBarTheme: AppBarTheme(
+    titleSpacing: 20.0,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: HexColor('333739'),
+      statusBarIconBrightness: Brightness.light,
+    ),
+    backgroundColor: HexColor('333739'),
+    elevation: 0.0,
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    ),
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Colors.red,
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    selectedItemColor: Colors.red,
+    unselectedItemColor: Colors.grey,
+    elevation: 20.0,
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: HexColor('333739'),
+  ),
+  textTheme: TextTheme(
+    bodyLarge: TextStyle(
+        fontSize: 18.0,
+        fontWeight: FontWeight.w600,
+        color: Colors.white
+    ),
+  ),
+);
+
+ThemeData lightTheme = ThemeData(
+  primarySwatch: Colors.red,
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: AppBarTheme(
+    titleSpacing: 20.0,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+    backgroundColor: Colors.white,
+    elevation: 0.0,
+    titleTextStyle: TextStyle(
+      color: Colors.black,
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.black,
+    ),
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Colors.red,
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    selectedItemColor: Colors.red,
+    unselectedItemColor: Colors.grey,
+    elevation: 20.0,
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: Colors.white,
+  ),
+  textTheme: TextTheme(
+    bodyLarge: TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  ),
+);
