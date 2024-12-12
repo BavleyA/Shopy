3031d3190dcfa46bcd0d3497bfbe9afb6870c378
BavleyA <paula3del@gmail.com>
2024-12-12 03:22:01 +0200

    handling sign out

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/modules/login/loging_screen.dart';
+import 'package:shop_app/shared/network/local/cache_helper.dart';
+import 'package:shop_app/shared/styles/colors/colors.dart';
 
 class HomeLayout extends StatelessWidget {
   const HomeLayout({super.key});
@@ -13,6 +16,23 @@ class HomeLayout extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      body: TextButton(
+          onPressed: (){
+            CacheHelper.removeData(key: 'token').then((value){
+              Navigator.pushAndRemoveUntil(context,
+                  MaterialPageRoute(builder: (context)=> LogingScreen()),
+                  (Route<dynamic> route) => false);
+            });
+          },
+          child: Text(
+              'Sign out'.toUpperCase(),
+            style: TextStyle(
+              color: defaultColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
       ),
     );
   }
--- lib/shared/network/local/cache_helper.dart
+++ lib/shared/network/local/cache_helper.dart
@@ -35,4 +35,12 @@ class CacheHelper{
     return await sharedPreferences.setDouble(key, value);
 
   }
+
+  static Future<bool> removeData({
+    required String key
+  }) async {
+
+    return await sharedPreferences.remove(key);
+
+  }
 }
