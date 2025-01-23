76ac8d340364e36007144d54d64757e71387c9db
BavleyA <paula3del@gmail.com>
2025-01-23 22:01:00 +0200

    add home page item builder

--- lib/modules/products/products_screen.dart
+++ lib/modules/products/products_screen.dart
@@ -1,6 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/models/home_model/home_model.dart';
 import 'package:shop_app/shared/styles/colors/colors.dart';
 import 'package:shop_app/shop_cubit/cubit.dart';
 import 'package:shop_app/shop_cubit/states.dart';
@@ -15,7 +17,7 @@ class ProductsScreen extends StatelessWidget {
       builder: (context , state) {
         return ConditionalBuilder(
           condition: ShopCubit.get(context).homeModel!=null,
-          builder: (context) => homeProductsBuilder(),
+          builder: (context) => homeProductsBuilder(ShopCubit.get(context).homeModel),
           fallback: (context) => const Center(child: CircularProgressIndicator(color: defaultColor,)),
         );
     },
@@ -23,9 +25,29 @@ class ProductsScreen extends StatelessWidget {
     );
   }
 
-  Widget homeProductsBuilder() => Column(
+  Widget homeProductsBuilder(HomeModel? model) => Column(
     children: [
+      CarouselSlider(
+        items: model?.data?.banners.map((element)=>
+          Image(
+            image: NetworkImage('${element.image}'),
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),).toList(),
+        options: CarouselOptions(
+          height: 200.0,
+          initialPage: 0,
+          viewportFraction: 1.0,
+          enableInfiniteScroll: true,
+          reverse: false,
+          autoPlay: true,
+          autoPlayInterval: const Duration(seconds: 3),
+          autoPlayAnimationDuration: const Duration(seconds: 1),
+          autoPlayCurve: Curves.fastOutSlowIn,
+          scrollDirection: Axis.horizontal,
 
+        ),
+      ),
     ],
   );
 }
