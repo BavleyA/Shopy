77129b68195f21dd16645471068abda3c92e427b
BavleyA <paula3del@gmail.com>
2025-01-23 21:44:56 +0200

    consumer in home page

--- lib/modules/products/products_screen.dart
+++ lib/modules/products/products_screen.dart
@@ -1,15 +1,31 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/shared/styles/colors/colors.dart';
+import 'package:shop_app/shop_cubit/cubit.dart';
+import 'package:shop_app/shop_cubit/states.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Products screen',
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
+    return BlocConsumer<ShopCubit,ShopStates>(
+      listener: (context,state) {},
+      builder: (context , state) {
+        return ConditionalBuilder(
+          condition: ShopCubit.get(context).homeModel!=null,
+          builder: (context) => homeProductsBuilder(),
+          fallback: (context) => const Center(child: CircularProgressIndicator(color: defaultColor,)),
+        );
+    },
+
     );
   }
+
+  Widget homeProductsBuilder() => Column(
+    children: [
+
+    ],
+  );
 }
