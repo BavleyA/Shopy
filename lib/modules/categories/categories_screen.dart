de8e820d1e1504d3f3e7ab18dde1a8bcb7c98cc2
BavleyA <paula3del@gmail.com>
2025-01-24 02:09:16 +0200

    finish categories screen

--- lib/modules/categories/categories_screen.dart
+++ lib/modules/categories/categories_screen.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/models/categories_model/categories_model.dart';
+import 'package:shop_app/shop_cubit/cubit.dart';
+import 'package:shop_app/shop_cubit/states.dart';
+
+import '../../models/categories_model/categories_model.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'categories screen',
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context , state ){},
+      builder: (context , state) {
+        return ListView.separated(
+          itemBuilder: (context , index) => buildCategoryItem(ShopCubit.get(context).categoriesModel!.data!.data[index]),
+          separatorBuilder: (context , index ) => const Divider(color: Colors.grey,),
+          itemCount: ShopCubit.get(context).categoriesModel!.data!.data.length,
+        );
+      },
     );
   }
+
+  Widget buildCategoryItem(DataModel model) => Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Row(
+      children: [
+        Image(image: NetworkImage(model.image!),
+          width: 100.0,
+          height: 100.0,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(width: 20.0,),
+        Text(
+          model.name!,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Spacer(),
+        Icon(
+          Icons.arrow_forward_ios,
+        ),
+      ],
+    ),
+  );
 }
