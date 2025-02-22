4581734d0285227e2d6de632e1a036b12e918280
BavleyA <paula3del@gmail.com>
2025-02-23 01:56:43 +0200

    favourites screen

--- lib/modules/favourites/Favourites_Screen.dart
+++ lib/modules/favourites/Favourites_Screen.dart
@@ -1,15 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shop_app/models/categories_model/categories_model.dart';
+import 'package:shop_app/models/home_model/home_model.dart';
+import 'package:shop_app/shared/styles/colors/colors.dart';
+import 'package:shop_app/shop_cubit/cubit.dart';
+import 'package:shop_app/shop_cubit/states.dart';
 
 class FavouritesScreen extends StatelessWidget {
   const FavouritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'favourites screen',
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
+    return Row(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: [
+            Image(
+              image: NetworkImage(''),
+              width: 120.0,
+              height: 120.0,
+            ),
+            if (1 != 0)
+              Container(
+                color: Colors.red,
+                padding: EdgeInsets.symmetric(horizontal: 5.0),
+                child: const Text(
+                  'DISCOUNT',
+                  style: TextStyle(
+                    fontSize: 10.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+          ],
+        ),
+        SizedBox(width: 20.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'iphone',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 14.0,
+                height: 1.3,
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  '100',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: defaultColor,
+                  ),
+                ),
+                const SizedBox(width: 5.0),
+                if (1 != 0)
+                  Text(
+                    '5000',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.grey,
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                const Spacer(),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    //ShopCubit.get(context).changFavourites(model.id!);
+                    //print(model.id);
+                  },
+                  icon: CircleAvatar(
+                    radius: 15.0,
+                    //backgroundColor: ShopCubit.get(context).isFav[model.id]! ? defaultColor : Colors.grey ,
+                    child: const Icon(
+                      Icons.favorite_outline_rounded,
+                      size: 14.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
